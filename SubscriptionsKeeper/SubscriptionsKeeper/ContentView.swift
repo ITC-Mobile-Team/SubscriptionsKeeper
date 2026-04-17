4957a88e432a18509033cffed31ac3b25efcab70
Bohdan Pokhidnia <bohdan.pokhidnia@itechcraft.com>
2026-04-17 11:55:41 +0300

    setup base project

---
+++ SubscriptionsKeeper/SubscriptionsKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubscriptionsKeeper
+//
+//  Created by Bohdan Pokhidnia on 17.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubscriptionsKeeper/SubscriptionsKeeper/SubscriptionsKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsKeeperApp.swift
+//  SubscriptionsKeeper
+//
+//  Created by Bohdan Pokhidnia on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionsKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
